9dddffda53de4120c335361a36008054774c7bf1
Kwkmts <87115759+Kwkmts@users.noreply.github.com>
2022-04-26 22:49:15 +0900

    initial commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const multer = require('multer');
+const upload = multer();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ greeting: `Hello ${name}!` });
+});
+
+app.post('/api/greeting', upload.none(), (req, res) => {
+  const name = req.body.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ greeting: `Hello ${name}!` });
+});
+
+app.listen(port, () => {
+  console.log(`Express server is running on localhost:${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,22 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [name2, setName2] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  const handleChange = (event, setValue) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmitGet = event => {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(name)}`)
+      .then(response => response.json())
+      .then(state => {
+        setGreeting(state.greeting);
+        setName('');
+      });
+  };
+
+  const handleSubmitPost = event => {
+    event.preventDefault();
+    const form = new FormData();
+    form.append('name', name2);
+
+    fetch('/api/greeting', {
+      method: 'POST',
+      body: form,
+    })
+      .then(response => response.json())
+      .then(state => {
+        setGreeting(state.greeting);
+        setName2('');
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>fetch/GET</h4>
+        <form onSubmit={handleSubmitGet}>
+          <label htmlFor="name">Enter your name: </label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={event => handleChange(event, setName)}
+            placeholder="fetch/GET"
+          />
+          <button type="submit">Submit</button>
+        </form>
+
+        <p>-----------------------------------------</p>
+
+        <h4>fetch/POST</h4>
+        <form onSubmit={handleSubmitPost}>
+          <label htmlFor="name">Enter your name: </label>
+          <input
+            id="name"
+            type="text"
+            value={name2}
+            onChange={event => handleChange(event, setName2)}
+            placeholder="fetch/POST"
+          />
+          <button type="submit">Submit</button>
+        </form>
+
+        <p>-----------------------------------------</p>
+
+        <p>{greeting}</p>
       </header>
     </div>
   );
